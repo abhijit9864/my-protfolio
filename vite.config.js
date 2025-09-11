09ca8dd15f9dccf34f6e402a6ba378920ab60c81
Abhijit Pradhan <Abhijit986@>
2025-09-11 10:59:32 +0530

    change spelling

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-protfolio/',
+  base: '/my-portfolio/',
   build: {
     outDir: 'dist',
   }
